16c05772e899eb47e4e751429f41e69431206a33
Juan J. San Emeterio <emeterio@Juans-MacBook-Pro.local>
2014-12-19 17:48:07 -0500

    Created test for creating seperate lists for seperate users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -34,8 +34,11 @@ class NewVisitorTest(LiveServerTestCase):
 		#She types "Buy Feathers"
 		inputbox.send_keys('Buy peacock feathers')
 
-		#When she hits enter, the page updates and the list now includes "Boy Feathers"
+		#When she hits enter, she is taken to a new URL
+		#and the page now lists 1: buy peacock feathers as an item in list
 		inputbox.send_keys(Keys.ENTER)
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		#The site invites her to enter another item, she enters "Buy a hat"
@@ -51,11 +54,37 @@ class NewVisitorTest(LiveServerTestCase):
 		self.check_for_row_in_list_table('2: Buy a hat')
 
 
-		#Cheryl wonders if the site will remember her list. The site has generated a url for her list
-		self.fail('finish the test!')
-		#She visits that URL and finds her to-do list in tact.
+		# a new user, Francis, goes to the site
 
-		#Satisfied, she goes back to sleep.
+		#we launch a new browser
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		#Francis visits the homepage and none of Edith's items are present
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('Buy a hat', page_text)
+
+
+		#Francis starts his own list
+		inputbox = self.browser.find_element_by_id('id_new_item')
+
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+
+		#Francis gets his own unique url
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url,edith_list_url)
+
+		#there is no trace of Edith's list
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
+
+		#Done
+
 
 
 
